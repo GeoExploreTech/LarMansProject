aadf9efc90b8d524aa3d17e845900a595f5503d4
Bolarinwa o. oluwaseun <bolarinwa@internal.interspacial.com>
2017-12-20 11:00:54 +0100

    e

--- src/app/login-page/login-page.component.ts
+++ src/app/login-page/login-page.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+declare let $ :any;
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
@@ -10,6 +10,42 @@ export class LoginPageComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    $(document)
+    .ready(function() {
+      $('.ui.form')
+        .form({
+          fields: {
+            email: {
+              identifier  : 'email',
+              rules: [
+                {
+                  type   : 'empty',
+                  prompt : 'Please enter your e-mail'
+                },
+                {
+                  type   : 'email',
+                  prompt : 'Please enter a valid e-mail'
+                }
+              ]
+            },
+            password: {
+              identifier  : 'password',
+              rules: [
+                {
+                  type   : 'empty',
+                  prompt : 'Please enter your password'
+                },
+                {
+                  type   : 'length[6]',
+                  prompt : 'Your password must be at least 6 characters'
+                }
+              ]
+            }
+          }
+        })
+      ;
+    });
+
   }
 
 }
